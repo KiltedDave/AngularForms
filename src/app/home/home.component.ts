601c3661ba6c4389a504fa28e6223d6290f9ac6e
David Moore <dpm4891@yahoo.com>
2018-12-28 14:51:10 -0600

    Completed chapter 4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import { Employee } from '../models/employee.model';
+import { clearModulesForTest } from '@angular/core/src/linker/ng_module_factory_loader';
 
 @Component({
   selector: 'home',
@@ -6,4 +8,13 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  languages = ['English','Spanish','Other'];
+  model = new Employee('Darla','Smith',true, 'w2','English');
+
+  firstNameToUpperCase(value:string){
+    if(value.length > 0)
+    this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    else
+    this.model.firstName = value;
+  }
 }
